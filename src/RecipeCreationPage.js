10187bb3a5797cc90fc3f6314f4229ff45e7a5b3
eddielugo <69050993+eddielugo@users.noreply.github.com>
2023-09-08 15:57:05 -0700

    Edited ingredients to JSON string with RecipeCreationPage.js

--- src/RecipeCreationPage.js
+++ src/RecipeCreationPage.js
@@ -1,36 +1,47 @@
 import React, { useState } from 'react';
 import './RecipeCreationPage.css';
 
-// TODO: ADD Error Boundary Component - see RecipeDetailPage.js for example
-
-
 // Represents the recipe creation page where users can input details for a new recipe.
 const RecipeCreationPage = () => {
-    // State variables for storing user input.
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [timeMinutes, setTimeMinutes] = useState('');
-    const [ingredients, setIngredients] = useState('');
+    // Initialize with one empty string for the first ingredient input
+    const [ingredientsList, setIngredientsList] = useState(['']);
     const [instructions, setInstructions] = useState('');
     const [image, setImage] = useState(null);
 
-// Handles the image file upload and sets the image state.
+    // Handles the image file upload and updates the image state.
     const handleImageUpload = (event) => {
         const file = event.target.files[0];
         setImage(file);
     }
-// Handles the form submission, logs the input values for now.
+
+    // Updates the specific ingredient value in the ingredientsList state.
+    const handleIngredientChange = (index, value) => {
+        const newIngredientsList = [...ingredientsList];
+        newIngredientsList[index] = value;
+        setIngredientsList(newIngredientsList);
+    }
+
+    // Adds a new empty ingredient input field by updating the ingredientsList state.
+    const addIngredientInput = () => {
+        setIngredientsList([...ingredientsList, '']);
+    }
+
+    // Handles the form submission by creating a recipe data object and sending a POST request.
     const handleSubmit = () => {
         const recipeData = {
             title: title,
             description: description,
             time_minutes: timeMinutes,
-            ingredients: ingredients,
+            ingredients: ingredientsList.join(', '), // Join the array into a single string
+            //ingredients: ingredientsList, // If the backend expects ingredients as an array, we can send ingredientsList directly
             instructions: instructions,
             image: image // Assuming the backend can handle base64 encoded images or a file path
         };
 
-        fetch('http://your-django-api-url/create-recipe/', {//TODO: Replace with our Django REST API URL
+        fetch('http://your-django-api-url/create-recipe/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -45,7 +56,6 @@ const RecipeCreationPage = () => {
             return response.json();
         })
         .then(data => {
-            // Handle successful form submission (e.g., navigate to another page or display a success message)
             console.log('Recipe successfully created:', data);
         })
         .catch(error => {
@@ -53,14 +63,29 @@ const RecipeCreationPage = () => {
         });
     }
 
+        /*This code allows users to dynamically add ingredient input fields by clicking the "Add Ingredient" button. 
+        Each ingredient's value is stored in the ingredientsList array, which is then used in the handleSubmit function 
+        to create the recipe data.*/
     return (
         <div className="recipe-creation-page">
             <h2>Create a Recipe</h2>
-              
             <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
             <textarea placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
             <input type="text" placeholder="Time (in minutes)" value={timeMinutes} onChange={(e) => setTimeMinutes(e.target.value)} />
-            <textarea placeholder="Ingredients" value={ingredients} onChange={(e) => setIngredients(e.target.value)} />
+            
+            {/* Map through ingredientsList and render an input for each ingredient */}
+            {ingredientsList.map((ingredient, index) => (
+                <input 
+                    key={index}
+                    type="text"
+                    placeholder={`Ingredient ${index + 1}`}
+                    value={ingredient}
+                    onChange={(e) => handleIngredientChange(index, e.target.value)}
+                />
+            ))}
+            {/* Button to add a new ingredient input field */}
+            <button onClick={addIngredientInput}>Add Ingredient</button>
+
             <textarea placeholder="Instructions" value={instructions} onChange={(e) => setInstructions(e.target.value)} />
             <input type="file" onChange={handleImageUpload} />
             <button onClick={handleSubmit}>Submit</button>
@@ -68,5 +93,5 @@ const RecipeCreationPage = () => {
     );
 }
 
-export default RecipeCreationPage;  // Export the RecipeCreationPage component for use in other parts of the application
 
+export default RecipeCreationPage;
