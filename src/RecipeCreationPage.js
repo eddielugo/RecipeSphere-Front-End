48eb98128bd4ab266e6a480300b6ce4d24b6b58e
eddielugo <69050993+eddielugo@users.noreply.github.com>
2023-09-08 15:32:04 -0700

    Added state variables for title, description, and timeMinutes. Updated the form fields to include inputs for title, description, and timeMinutes. Modified the handleSubmit function to send the data as JSON and included the Authorization header.

--- src/RecipeCreationPage.js
+++ src/RecipeCreationPage.js
@@ -1,81 +1,68 @@
 import React, { useState } from 'react';
 import './RecipeCreationPage.css';
 
-
 // TODO: ADD Error Boundary Component - see RecipeDetailPage.js for example
 
 
 // Represents the recipe creation page where users can input details for a new recipe.
 const RecipeCreationPage = () => {
     // State variables for storing user input.
-    const [recipeName, setRecipeName] = useState('');
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [timeMinutes, setTimeMinutes] = useState('');
     const [ingredients, setIngredients] = useState('');
     const [instructions, setInstructions] = useState('');
     const [image, setImage] = useState(null);
 
-    // Handles the image file upload and sets the image state.
+// Handles the image file upload and sets the image state.
     const handleImageUpload = (event) => {
         const file = event.target.files[0];
         setImage(file);
     }
-
-    // Handles the form submission, logs the input values for now.
+// Handles the form submission, logs the input values for now.
     const handleSubmit = () => {
-        // Log the input values (commented out)
-        // console.log(recipeName, ingredients, instructions, image);
+        const recipeData = {
+            title: title,
+            description: description,
+            time_minutes: timeMinutes,
+            ingredients: ingredients,
+            instructions: instructions,
+            image: image // Assuming the backend can handle base64 encoded images or a file path
+        };
 
-        // Create FormData object to hold the form data
-        const formData = new FormData();
-        formData.append('recipeName', recipeName);
-        formData.append('ingredients', ingredients);
-        formData.append('instructions', instructions);
-        formData.append('image', image);
-
-        fetch('http://your-django-api-url/create-recipe/', { //TODO: Replace with your Django REST API URL
+        fetch('http://your-django-api-url/create-recipe/', {//TODO: Replace with our Django REST API URL
             method: 'POST',
-            body: formData
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${localStorage.getItem('token')}`
+            },
+            body: JSON.stringify(recipeData)
         })
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
-        }
+            }
             return response.json();
         })
         .then(data => {
-    // Handle successful form submission (e.g., navigate to another page or display a success message)
-    console.log('Recipe successfully created:', data);
+            // Handle successful form submission (e.g., navigate to another page or display a success message)
+            console.log('Recipe successfully created:', data);
         })
         .catch(error => {
-    // Handle errors (e.g., display an error message)
-    console.log('Error creating recipe:', error);
+            console.log('Error creating recipe:', error);
         });
     }
 
     return (
         <div className="recipe-creation-page">
             <h2>Create a Recipe</h2>
-            {/* Input field for the recipe name */}
-            <input 
-                type="text" 
-                placeholder="Recipe Name" 
-                value={recipeName} 
-                onChange={(e) => setRecipeName(e.target.value)} 
-            />
-            {/* Textarea for entering the ingredients */}
-            <textarea 
-                placeholder="Ingredients" 
-                value={ingredients} 
-                onChange={(e) => setIngredients(e.target.value)} 
-            />
-            {/* Textarea for entering the cooking instructions */}
-            <textarea 
-                placeholder="Instructions" 
-                value={instructions} 
-                onChange={(e) => setInstructions(e.target.value)} 
-            />
-            {/* Input for uploading an image for the recipe */}
+              
+            <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+            <textarea placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
+            <input type="text" placeholder="Time (in minutes)" value={timeMinutes} onChange={(e) => setTimeMinutes(e.target.value)} />
+            <textarea placeholder="Ingredients" value={ingredients} onChange={(e) => setIngredients(e.target.value)} />
+            <textarea placeholder="Instructions" value={instructions} onChange={(e) => setInstructions(e.target.value)} />
             <input type="file" onChange={handleImageUpload} />
-            {/* Button to submit the form */}
             <button onClick={handleSubmit}>Submit</button>
         </div>
     );
