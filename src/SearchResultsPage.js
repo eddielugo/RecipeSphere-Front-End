461c450625c479051948e15fcf317188d043a169
eddielugo <69050993+eddielugo@users.noreply.github.com>
2023-09-08 17:32:30 -0700

    Updated the SearchResultsPage.js file: Removed the commented-out sampleData. Updated the useEffect in SearchResultsPage: First, it fetches the tag ID based on the search query. Then, it fetches the recipes based on the tag ID. Added inline comments to explain the logic. This should now fetch the recipes based on the tag ID retrieved from the search query.

--- src/SearchResultsPage.js
+++ src/SearchResultsPage.js
@@ -2,64 +2,69 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './SearchResultsPage.css';
 
-// TODO: ADD Error Boundary Component - see RecipeDetailPage.js for example
-
-
 // Main component to display the search results page.
 const SearchResultsPage = () => {
     const [searchQuery, setSearchQuery] = useState('');
-    // const [results, setResults] = useState(sampleData); // Commented out for demonstration purposes only
-    const [results, setResults] = useState([]); // Initialize with empty arrayults with sample data. Replace with actual data fetching logic in a real-world scenario.
-    
+    const [results, setResults] = useState([]);
+
     useEffect(() => {
-        // TODO: Fetch search results from Django REST API when searchQuery changes
         if (searchQuery) {
+            // First, fetch the tag ID based on the search query
             fetch(`https://be.recipesphere.net/api/tags/?name=${searchQuery}`, {
                 headers: {
                     'Authorization': `Token ${localStorage.getItem('token')}`
-                  }
-                })
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    console.log(response)
-                    return response.json();
-                })
-                .then(data => setResults(data))
-                .catch(error => console.error('Error fetching search results:', error));
+                }
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                // Assuming the API returns an array and we're interested in the first result
+                const tagId = data[0]?.id;
+                if (tagId) {
+                    // Fetch recipes based on the tag ID
+                    return fetch(`https://be.recipesphere.net/api/recipe/?tags=${tagId}`, {
+                        headers: {
+                            'Authorization': `Token ${localStorage.getItem('token')}`
+                        }
+                    });
+                }
+                throw new Error('Tag not found');
+            })
+            .then(response => response.json())
+            .then(data => setResults(data))
+            .catch(error => console.error('Error fetching search results:', error));
         }
     }, [searchQuery]);
 
-            // Function to handle the search logic.
     const handleSearch = () => {
-       // Implement search logic here (if needed)
         // The useEffect will handle fetching based on searchQuery
     }
 
     return (
         <div className="search-results-page">
-            {/* Search bar component */}
+        {/* Search bar component */}
             <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} handleSearch={handleSearch} />
-            
-            {/* List of search results */}
+             {/* List of search results */}
             <SearchResultsList results={results} />
         </div>
     );
 }
 
-// Component for the search bar.
 const SearchBar = ({ searchQuery, setSearchQuery, handleSearch }) => {
     return (
         <div className="search-bar">
-            {/* Input field for search queries */}
+          {/* Input field for search queries */}
             <input 
                 type="text" 
                 value={searchQuery} 
-                onChange={(e) => setSearchQuery(e.target.value)} // Update the search query state on input change
+                onChange={(e) => setSearchQuery(e.target.value)}
                 placeholder="Search for recipes..." 
             />
-            {/* Button to initiate the search */}
+             {/* Button to initiate the search */}
             <button onClick={handleSearch}>Search</button>
         </div>
     );
@@ -69,27 +74,23 @@ const SearchBar = ({ searchQuery, setSearchQuery, handleSearch }) => {
 const SearchResultsList = ({ results }) => {
     return (
         <div className="search-results-list">
-            {/* Map through the results and display each recipe */}
+         {/* Map through the results and display each recipe */}
             {results.map(recipe => (
                 <RecipeCard key={recipe.id} recipe={recipe} />
             ))}
         </div>
     );
 }
-
 // Component to display individual recipe cards.
 const RecipeCard = ({ recipe }) => {
     return (
         <div className="recipe-card">
-            {/* Display recipe thumbnail */}
+        {/* Display recipe thumbnail */}
             <img src={recipe.thumbnail} alt={recipe.name} />
-            
-            {/* Display recipe name */}
+             {/* Display recipe name */}
             <h3>{recipe.name}</h3>
-            
-            {/* Display recipe description */}
+             {/* Display recipe description */}
             <p>{recipe.description}</p>
-            
             {/* Link to view the full recipe details */}
             <Link to={`/recipe-detail/${recipe.id}`}>
                 <button>View Recipe</button>
@@ -97,14 +98,6 @@ const RecipeCard = ({ recipe }) => {
         </div>
     );
 }
-
-// Sample data for demonstration purposes.
-//const sampleData = [
-//    { id: 1, name: 'Recipe A', description: 'Description A', thumbnail: 'imageA.jpg' },
-//    { id: 2, name: 'Recipe B', description: 'Description B', thumbnail: 'imageB.jpg' },
-//    { id: 3, name: 'Recipe C', description: 'Description C', thumbnail: 'imageC.jpg' },
-//    { id: 4, name: 'Recipe D', description: 'Description D', thumbnail: 'imageD.jpg' } // Added fourth recipe
-//];
 
 export default SearchResultsPage; // Export the SearchResultsPage component for use in other parts of the application.
 
