abde0d506c8902b7c7f7b76e49831519ff882493
eddielugo <69050993+eddielugo@users.noreply.github.com>
2023-09-16 13:29:45 -0700

    Changes to RecipeCreationPage: The ingredientsList state is now an array of objects, where each object has a key and a value; The handleIngredientChange function now updates the ingredient value based on the unique key; The addIngredientInput function generates a new ingredient input with a unique key; The key prop for each ingredient input is set to the unique key from the ingredient object ensures that each ingredient input has a stable and unique key, addressing the SonarLint warning (more flexible data structure for managing ingredients); Added formData.append functions for ingredients, instructions, and image; Added comments for Return function

--- src/RecipeCreationPage.js
+++ src/RecipeCreationPage.js
@@ -6,8 +6,8 @@ const RecipeCreationPage = () => {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [timeMinutes, setTimeMinutes] = useState('');
-    // Initialize with one empty string for the first ingredient input
-    const [ingredientsList, setIngredientsList] = useState(['']);
+    // Initialize with one object for the first ingredient input
+    const [ingredientsList, setIngredientsList] = useState([{ key: Date.now(), value: '' }]);
     const [instructions, setInstructions] = useState('');
     const [image, setImage] = useState(null);
 
@@ -18,15 +18,16 @@ const RecipeCreationPage = () => {
     }
 
     // Updates the specific ingredient value in the ingredientsList state.
-    const handleIngredientChange = (index, value) => {
-        const newIngredientsList = [...ingredientsList];
-        newIngredientsList[index] = value;
+    const handleIngredientChange = (key, value) => {
+        const newIngredientsList = ingredientsList.map(ingredient => 
+            ingredient.key === key ? { ...ingredient, value } : ingredient
+        );
         setIngredientsList(newIngredientsList);
     }
 
-    // Adds a new empty ingredient input field by updating the ingredientsList state.
+    // Adds a new ingredient input field with a unique key
     const addIngredientInput = () => {
-        setIngredientsList([...ingredientsList, '']);
+        setIngredientsList([...ingredientsList, { key: Date.now() + Math.random(), value: '' }]);
     }
 
     // Handles the form submission by creating a recipe data object and sending a POST request.
@@ -36,7 +37,10 @@ const RecipeCreationPage = () => {
         formData.append('title', title);
         formData.append('description', description);
         formData.append('time_minutes', timeMinutes);
-        
+        formData.append('ingredients', ingredientsList);
+        formData.append('instructions', instructions);
+        formData.append('image', image);
+
         // Convert the ingredientsList array to a JSON string and append it
         let jsonIngredients = {};
 
@@ -72,10 +76,25 @@ const RecipeCreationPage = () => {
         });
     }
     
-
-        /*This code allows users to dynamically add ingredient input fields by clicking the "Add Ingredient" button. 
-        Each ingredient's value is stored in the ingredientsList array, which is then used in the handleSubmit function 
-        to create the recipe data.*/
+/* 
+ * This return function renders the recipe creation form. 
+ * 
+ * The ingredients input fields are dynamically generated based on the ingredientsList state. 
+ * Each ingredient in the ingredientsList is an object with a unique key and a value. 
+ * The unique key ensures stable rendering and addresses the SonarLint warning about using array indices as keys.
+ * 
+ * The unique key for each ingredient is generated using a combination of the current timestamp and a random number. 
+ * This ensures that even if ingredients are added in quick succession, they will have distinct keys.
+ * 
+ * When an ingredient's value is updated, the handleIngredientChange function is called with the ingredient's unique key 
+ * and the new value. This allows us to update the specific ingredient in the ingredientsList state without affecting others.
+ * 
+ * The addIngredientInput function adds a new ingredient input field to the form by updating the ingredientsList state 
+ * with a new ingredient object.
+ * 
+ * This approach provides a flexible and scalable way to manage dynamic input fields in the form and ensures stable rendering 
+ * of the ingredient inputs.
+ */
     return (
         <div className="recipe-creation-page">
             <h2>Create a Recipe</h2>
@@ -84,13 +103,13 @@ const RecipeCreationPage = () => {
             <input type="text" placeholder="Time (in minutes)" value={timeMinutes} onChange={(e) => setTimeMinutes(e.target.value)} />
             
             {/* Map through ingredientsList and render an input for each ingredient */}
-            {ingredientsList.map((ingredient, index) => (
+            {ingredientsList.map(ingredient => (
                 <input 
-                    key={index}
+                    key={ingredient.key}
                     type="text"
-                    placeholder={`Ingredient ${index + 1}`}
-                    value={ingredient}
-                    onChange={(e) => handleIngredientChange(index, e.target.value)}
+                    placeholder={`Ingredient`}
+                    value={ingredient.value}
+                    onChange={(e) => handleIngredientChange(ingredient.key, e.target.value)}
                 />
             ))}
             {/* Button to add a new ingredient input field */}
