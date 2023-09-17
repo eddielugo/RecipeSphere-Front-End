d96b9e08d12a0f608e5aaec7b2bccb2d14486cfe
eddielugo <69050993+eddielugo@users.noreply.github.com>
2023-09-16 18:13:33 -0700

    Edited function names on ProfilePage.js, created hyperlink to AddRecipePage.js for both MyRecipe and FavoriteRecipe lists

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,20 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import './ProfilePage.css';
 
-// TODO: ADD Error Boundary Component - see RecipeDetailPage.js for example
 
 
-
-// Main Profile Page Component
 const ProfilePage = () => {
     return (
         <div className="profile-page">
-            {/* User's personal information section */}
+        {/* User's personal information section */}
             <UserInformation />
             {/* List of recipes shared by the user */}
-            <SharedRecipes />
+            <MyRecipes />
             {/* List of recipes saved or favorited by the user */}
-            <SavedRecipes />
+            <FavoriteRecipes />
         </div>
     );
 }
@@ -23,121 +21,125 @@ const ProfilePage = () => {
 const UserInformation = () => {
     const [userInfo, setUserInfo] = useState({});
 
-    // TODO: Change to our URL
+// Fetch user information from Django REST API
     useEffect(() => {
         fetch('https://be.recipesphere.net/api/profile/', {
             headers: {
                 'Authorization': `Token ${window.sessionStorage.getItem('token')}`
-              }
-            }) // Change this to your Django API URL
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => setUserInfo(data))
-            .catch(error => console.error('Error fetching user information:', error));
-    }, []);
-   // Function to handle profile image upload
-   //TODO: remove image uplaod and display backend does not support profile pics yet. I could do it in the future
-    const handleImageUpload = (event) => {
-        const file = event.target.files[0];
-        console.log(file);
-        // Handle the image upload logic here (e.g., send to server or store locally)
-    }
-
-    return (
-        <div className="user-info">
-            <h2>User Information</h2>
-            <div className="profile-picture">
-                {console.log(userInfo)}
-                {/* TODO: Display user's profile picture */}
-                <img src={userInfo.profilePicture || "../Images/ProfileAvatar.jpg"} alt="User Profile" />
-                {/* Input for uploading a new profile picture */}
-                <input type="file" onChange={handleImageUpload} />
-            </div>
-            {/* Sample data for demonstration */}
-            {/* TODO: userInfo.user.username and userInfo.user.email work if you first load page with the original code and then change it to this 
-            So i think something is wrong with the loading of the info. this work if you run this page with the commented
-            code below and one it load uncomment and the correct username and email show*/}
-
-            {/* <p><strong>Username:</strong>{userInfo.user.username || 'JohnDoe'}</p>
-            <p><strong>Email:</strong> {userInfo.user.email || 'johndoe@example.com'}</p> */}
-            
-        </div>
-    );
-}
-
-// Component to display recipes shared by the user
-//TODO: remname to MyRecipes and add end point
-const SharedRecipes = () => {
-    const [sharedRecipes, setSharedRecipes] = useState([]);
-// Fetch shared recipes from Django REST API
-useEffect(() => {
-    fetch('https://be.recipesphere.net/api/recipe/my_recipes/', {
-            headers: {
-                'Authorization': `Token ${window.sessionStorage.getItem('token')}`
-              }
-            }) // Change this to your Django API URL
+            }
+        })
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
             return response.json();
         })
-        .then(data => setSharedRecipes(data))
-        .catch(error => console.error('Error fetching shared recipes:', error));
-}, []);
+        .then(data => setUserInfo(data))
+        .catch(error => console.error('Error fetching user information:', error));
+    }, []);
 
-// Sample data for demonstration (commented out)
-// const recipes = ['Recipe 1', 'Recipe 2', 'Recipe 3'];
+    // Function to handle profile image upload
+   //Commented outimage uplaod and display backend does not support profile pics yet. Do it in the future
+    //const handleImageUpload = (event) => {
+        //const file = event.target.files[0];
+        //console.log(file);
+        // Handle the image upload logic here (e.g., send to server or store locally)
+    //}
+
 
     return (
-        <div className="shared-recipes">
-            <h2>Shared Recipes</h2>
-            {/* List of shared recipes */}
+        <div className="user-info">
+            <h2>User Information</h2>
+              {/* Display user's profile picture-future add */}
+            {/*<img src={userInfo.profilePicture || "../Images/ProfileAvatar.jpg"} alt="User Profile" />*/}
+            {/* : userInfo.user.username and userInfo.user.email work if you first load page with the original code and then change it to this*/} 
+            {/*So i think something is wrong with the loading of the info. this work if you run this page with the commented*/}
+            {/*code below and one it load uncomment and the correct username and email show*/}
+            {/*<p><strong>Username:</strong> {userInfo.user?.username || 'JohnDoe'}</p>*/}
+            {/*<p><strong>Email:</strong> {userInfo.user?.email || 'johndoe@example.com'}</p>  */}
+        </div>
+    
+    );
+ }
+// Component to display recipes shared by the user
+const MyRecipes = () => {
+    const [myRecipes, setMyRecipes] = useState([]);
+    
+// Fetch shared recipes from Django REST API
+    useEffect(() => {
+        fetch('https://be.recipesphere.net/api/api/my_recipes/', {
+            headers: {
+                'Authorization': `Token ${window.sessionStorage.getItem('token')}`
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => setMyRecipes(data))
+        .catch(error => console.error('Error fetching my recipes:', error));
+    }, []);
+
+    return (
+        <div className="my-recipes">
+            <h2>My Recipes</h2>
+            {/* List of "My" recipes */}
             <ul>
-            {/*TODO: add edit recipe button here*/}
-            {sharedRecipes.map(recipe => <li key={recipe.id}>{recipe.title}</li>)}
+                {myRecipes.map(recipe => (
+                    <li key={recipe.id}>
+                        {/* Wrap the recipe title in a Link component to route to EditRecipePage */}
+                        <Link to={`/EditRecipePage/${recipe.id}`}>{recipe.title}</Link>
+                    </li>
+                ))}
             </ul>
         </div>
     );
 }
 
 // Component to display recipes favorited by the user
-//TODO: rename as FavoriteRecipes where is lists favorites and are hyperlinked (like on homepage) and open in
-// a recipedetail format; add end point for favorited recipes
-const SavedRecipes = () => {
-    const [savedRecipes, setSavedRecipes] = useState([]);
+const FavoriteRecipes = () => {
+    const [favoriteRecipes, setFavoriteRecipes] = useState([]);
 
-   // Fetch saved recipes from Django REST API
-   useEffect(() => {
-    fetch('https://be.recipesphere.net/api/profile/', {
+// Fetch favorite recipes from Django REST API
+    useEffect(() => {
+        fetch('https://be.recipesphere.net/api/api/profile/', {
             headers: {
                 'Authorization': `Token ${window.sessionStorage.getItem('token')}`
-              }
-            }) // Change this to your Django API URL
-        .then(data => setSavedRecipes(data))
-        .catch(error => console.error('Error fetching saved recipes:', error));
-}, []);
-
-
-    // Sample data for demonstration (commented out)
-    // const recipes = ['Recipe A', 'Recipe B', 'Recipe C'];
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.favorites) {
+                setFavoriteRecipes(data.favorites);
+            }
+        })
+        .catch(error => console.error('Error fetching favorite recipes:', error));
+    }, []);
 
     return (
-        <div className="saved-recipes">
-            <h2>Saved/Favorite Recipes</h2>
-            {/* List of saved or favorited recipes */}
+        <div className="favorite-recipes">
+            <h2>Favorite Recipes</h2>
+            {/* List of favorited recipes */}
             <ul>
             {/* TODO: Fix map function from json object field favorites from response of profile*/}
-            {console.log(savedRecipes)}
-            {/* {savedRecipes.favorites.map(recipe => <li key={recipe.id}>{recipe.title}</li>)} */}
+                {favoriteRecipes.map(recipe => (
+                    <li key={recipe.id}>
+                        {/* Wrap the recipe title in a Link component */}
+                        <Link to={`/EditRecipePage/${recipe.id}`}>{recipe.title}</Link>
+                    </li>
+                ))}
             </ul>
         </div>
     );
 }
 
-export default ProfilePage;  // Export the ProfilePage component for use in other parts of the application
+
+export default ProfilePage;
 
